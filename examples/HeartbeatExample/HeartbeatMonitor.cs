84bbfe9fad35fbc2ea6b73f137741091f97a9daf
martinalmstrom <martin.almstrom@collectorbank.se>
2017-10-30 22:19:37 +0100

    Added example

---
+++ examples/HeartbeatExample/HeartbeatMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Collector.Common.Heartbeat;
+using Microsoft.Extensions.Logging;
+
+namespace HeartbeatExample
+{
+    public class HeartbeatMonitor : IHeartbeatMonitor
+    {
+        private readonly ILogger _logger;
+
+        public HeartbeatMonitor(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger<HeartbeatMonitor>();
+        }
+        public Task RunHealthCheckAsync()
+        {
+            _logger.LogInformation("Doint stuff");
+            return Task.CompletedTask;
+        }
+    }
+}
